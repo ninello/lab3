9a28ef96f60b18f0eece3521cbeac66f50e10ccf
student <aleksandramay2711@gmail.com>
2023-02-28 16:51:16 +0300

    lab2

---
+++ main.py
@@ -0,0 +1,18 @@
+def proc1():
+    n = int(input('Введите нужное количество слов: '))
+    result = ''
+    for i in range(n):
+        x = input(f"ввод слов {i + 1}:")
+        result += x + " "
+    print("результат:", result.strip())
+
+
+def proc2():
+
+    result = []
+
+    while (wd := str(input())) != "stop":
+          result.append(wd)
+    print(" ".join(result))
+proc1(), proc2()
+
